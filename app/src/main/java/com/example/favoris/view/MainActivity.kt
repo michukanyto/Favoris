fe9eab160c77f682d92a5b2de9351638958c3947
michukanyto <escobar.marlon@gmail.com>
2020-02-10 21:06:04 -0500

    cleaning code

--- app/src/main/java/com/example/favoris/view/MainActivity.kt
+++ app/src/main/java/com/example/favoris/view/MainActivity.kt
@@ -2,27 +2,15 @@ package com.example.favoris.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.*
-import com.example.favoris.App
 import com.example.favoris.R
-import com.example.favoris.dao.IBookmarkDao
-import com.example.favoris.dao.IFolderDao
-import com.example.favoris.model.Bookmark
 import com.example.favoris.model.Folder
 import com.example.favoris.viewmodel.FavoriViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.Default
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        lateinit var liveDataOwner: LifecycleOwner
-    }
 
     private var folderNameLiveData = MutableLiveData<String>()
     lateinit var getFolderLiveData: LiveData<Folder>
@@ -36,12 +24,10 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProviders.of(this).get(FavoriViewModel::class.java)
 
         getFolderLiveData = Transformations.switchMap(folderNameLiveData){ name ->
-//            folderDAO.getFolder(name)
             viewModel.getFolder(name)
         }
 
         getFolderLiveData.observe(this, Observer { folder ->
-//            createBookmark(folder!!.id,bookmarkFolderNameEditText.textString(),bookmarkUrlEditText.textString())
             viewModel.createBookMark(folder!!.id,bookmarkFolderNameEditText.textString(),bookmarkUrlEditText.textString())
             displayToast("Bookmark created successfully")
             viewModel.getBookmarks().observe(this, Observer {bookmarks ->
@@ -51,14 +37,11 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-
         //CREATE FOLDER
         createFolderButton.setOnClickListener {
-            viewModel.saveFolder(createFolderEditText.textString()!!)
-//            /////////première solution
-//            viewModel.getState().observe(this, Observer { state ->
-            /////////sln with transformation
-            viewModel.getState2().observe(this, Observer {
+            viewModel.saveFolder(createFolderEditText.textString())
+
+            viewModel.getState().observe(this, Observer {
                 if (it) {
                     displayToast("Folder created successfully")
                 } else {
@@ -66,14 +49,12 @@ class MainActivity : AppCompatActivity() {
                 }
             })
             bookmarkFolderNameEditText.setText(createFolderEditText.textString())
-
         }
 
         //CREATE BOOKMARK
         createBookmarkButton.setOnClickListener {
             folderNameLiveData.value = bookmarkFolderNameEditText.textString()
         }
-
     }
 
     fun displayToast(message: String) {
--- app/src/main/java/com/example/favoris/viewmodel/FavoriViewModel.kt
+++ app/src/main/java/com/example/favoris/viewmodel/FavoriViewModel.kt
@@ -1,18 +1,15 @@
 package com.example.favoris.viewmodel
 
-import android.util.Log
-import android.view.animation.Transformation
+
 import androidx.lifecycle.*
 import com.example.favoris.App
 import com.example.favoris.dao.IBookmarkDao
 import com.example.favoris.dao.IFolderDao
 import com.example.favoris.model.Bookmark
 import com.example.favoris.model.Folder
-import com.example.favoris.view.MainActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.launch
-import kotlin.random.Random
 
 class ViewModelState(val success:Boolean = false)
 
@@ -20,39 +17,20 @@ class FavoriViewModel : ViewModel() {
 
     private val folderDAO: IFolderDao = App.dataBase.folderDAO()
     private val bookmarkDAO: IBookmarkDao = App.dataBase.bookmarkDAO()
-    /////////sln with transformation
     private lateinit var folderLiveData: LiveData<Boolean>
-    /////////sln 2
-//    private val lifecycleOwner: LifecycleOwner? = context as? LyfecycleOwner
 
     fun saveFolder(folderName: String) {
         CoroutineScope(Default).launch {
             folderDAO.insertFolder(Folder(name = folderName))
         }
 
-/////////sln with transformation
         folderLiveData = Transformations.map(folderDAO.getFolder(folderName)){
-//            folderwasCreated(folderName, it)
             folderName == it.name
         }
-
-        //première solution
-//        folderDAO.getFolder(folderName).observe(MainActivity.liveDataOwner, Observer {folder ->
-//            if (folderName == folder.name) {
-//                success.value = ViewModelState(true)
-//            } else {
-//                success.value = ViewModelState(false)
-//            }
-//        })
     }
 
-    //première solution
-//    private val success = MutableLiveData<ViewModelState>()
 
-    //première solution
-//    fun getState(): LiveData<ViewModelState> = success
-
-    fun getState2() : LiveData<Boolean> = folderLiveData
+    fun getState() : LiveData<Boolean> = folderLiveData
 
     fun createBookMark(folderId: Long,bmName: String,bmUrl: String) {
         CoroutineScope(Default).launch {
